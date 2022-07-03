0304846817dd2dbd758b9659672a2a0951278a19
jerry73204 <jerry73204@gmail.com>
2022-07-04 01:59:44 +0800

    Rewrite send_forget_sourced_subscription_to_net_childs()

--- zenoh/src/net/routing/pubsub.rs
+++ zenoh/src/net/routing/pubsub.rs
@@ -371,32 +371,32 @@ fn send_forget_sourced_subscription_to_net_childs(
     src_face: Option<&Arc<FaceState>>,
     routing_context: Option<RoutingContext>,
 ) {
-    for child in childs {
-        if net.graph.contains_node(*child) {
-            match faces
-                .values()
-                .find(|face| face.pid == net.graph[*child].pid)
-                .cloned()
-            {
-                Some(someface) => {
-                    if src_face.is_none() || someface.id != src_face.unwrap().id {
-                        let key_expr = Tables::decl_key(restree, res, &someface);
+    childs
+        .iter()
+        .filter_map(|&child| net.graph.node_weight(child))
+        .filter_map(|child_node| {
+            let someface = faces.values().find(|face| face.pid == child_node.pid);
 
-                        log::debug!(
-                            "Send forget subscription {} on {}",
-                            restree.expr(res),
-                            someface
-                        );
-
-                        someface
-                            .primitives
-                            .forget_subscriber(&key_expr, routing_context);
-                    }
-                }
-                None => log::trace!("Unable to find face for pid {}", net.graph[*child].pid),
+            if someface.is_none() {
+                log::trace!("Unable to find face for pid {}", child_node.pid);
             }
-        }
-    }
+
+            someface
+        })
+        .filter(|someface| !matches!(src_face, Some(face) if face.id == someface.id))
+        .for_each(|someface| {
+            let key_expr = Tables::decl_key(restree, res, someface);
+
+            log::debug!(
+                "Send forget subscription {} on {}",
+                restree.expr(res),
+                someface
+            );
+
+            someface
+                .primitives
+                .forget_subscriber(&key_expr, routing_context);
+        });
 }
 
 fn propagate_forget_simple_subscription(tables: &mut Tables, res: &ResourceTreeIndex) {
