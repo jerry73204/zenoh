2cc178b32d979c0d4f706b4d972bd7e650679ac1
jerry73204 <jerry73204@gmail.com>
2022-07-06 17:02:19 +0800

    Rewrite propagate_sourced_subscription()

--- zenoh/src/net/routing/pubsub.rs
+++ zenoh/src/net/routing/pubsub.rs
@@ -111,34 +111,41 @@ fn propagate_sourced_subscription(
     let restree = &mut tables.restree;
     let net = net!(tables, net_type).unwrap();
 
-    match net.get_node_from_pid(*source) {
-        Some((tree_sid, _)) => {
-            if let Some(tree) = net.trees.get(tree_sid.index()) {
-                send_sourced_subscription_to_net_childs(
-                    restree,
-                    &tables.faces,
-                    net,
-                    &tree.childs,
-                    res,
-                    src_face,
-                    sub_info,
-                    Some(RoutingContext::new(tree_sid.index() as ZInt)),
-                );
-            } else {
-                log::trace!(
-                    "Propagating sub {}: tree for node {} sid:{} not yet ready",
-                    tables.restree.expr(res),
-                    tree_sid.index(),
-                    source
-                );
-            }
+    let tree_sid = match net.get_node_from_pid(*source) {
+        Some((tree_sid, _)) => tree_sid,
+        None => {
+            log::error!(
+                "Error propagating sub {}: cannot get index of {}!",
+                tables.restree.expr(res),
+                source
+            );
+            return;
         }
-        None => log::error!(
-            "Error propagating sub {}: cannot get index of {}!",
-            tables.restree.expr(res),
-            source
-        ),
-    }
+    };
+
+    let tree = match net.trees.get(tree_sid.index()) {
+        Some(tree) => tree,
+        None => {
+            log::trace!(
+                "Propagating sub {}: tree for node {} sid:{} not yet ready",
+                tables.restree.expr(res),
+                tree_sid.index(),
+                source
+            );
+            return;
+        }
+    };
+
+    send_sourced_subscription_to_net_childs(
+        restree,
+        &tables.faces,
+        net,
+        &tree.childs,
+        res,
+        src_face,
+        sub_info,
+        Some(RoutingContext::new(tree_sid.index() as ZInt)),
+    );
 }
 
 fn register_router_subscription(
