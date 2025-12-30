043e9c7ae6ca46c39c6a67071852d0a8eb4874f7
kaiii708 <yuvivian0708@gmail.com>
2025-12-30 22:56:55 +0800

    Fix router presubscription to also check target_router in router_subs
    Ensure declarePresubscribe is sent when target router is missing from
    the presubscribe control message subscription list

--- zenoh/src/net/routing/hat/router/pubsub.rs
+++ zenoh/src/net/routing/hat/router/pubsub.rs
@@ -556,7 +556,7 @@ fn register_router_presubscription(
     tracing::trace!("register_router_presubscription");
     tracing::trace!("{:?}", res);
     tracing::trace!("{:?}", res_hat!(res).router_subs);
-    if !res_hat!(res).router_subs.contains(&router) {
+    if !res_hat!(res).router_subs.contains(&router) || !res_hat!(res).router_subs.contains(&target_router) {
         // Register the presubscription
         {
             tracing::trace!("router: {:?}", router);
